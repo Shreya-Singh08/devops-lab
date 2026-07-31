e6fae48a6756b226fc8f827183e37742af767869
Shreya-Singh08 <1997.21shreya@gmail.com>
2026-07-31 12:02:57 +0530

    Update health endpoint to test CI/CD pipeline

--- app/app.py
+++ app/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "ok"}, 200
-
+    return {"status": "ok", "version": "v2"}, 200
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
