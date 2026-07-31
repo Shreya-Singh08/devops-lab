095fa1718709e1dca38ff3bc5710b3ea8f366db4
Shreya-Singh08 <1997.21shreya@gmail.com>
2026-07-31 11:02:17 +0530

    Add ECR module, push app image

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -6,3 +6,8 @@ module "network" {
   source  = "../../modules/network"
   project = "devops-lab"
 }
+
+module "ecr" {
+  source  = "../../modules/ecr"
+  project = "devops-lab"
+}
---
+++ infra/modules/ecr/variables.tf
@@ -0,0 +1,4 @@
+variable "project" {
+  description = "Project name used for tagging resources"
+  type        = string
+}
